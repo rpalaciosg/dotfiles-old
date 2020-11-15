b641bfe72affeb0cbb0b20970cd5f9a615c165fc
Richard Palacios G <apalaciosg91@gmail.com>
2020-11-15 03:14:50 -0500

    fonts: update font to cascadia

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,14 +1,28 @@
 # Download and install the latast version of jetbrains mono.
 # https://www.jetbrains.com/lp/mono/
 
-FONT_NAME=JetBrainsMono-2.001.zip
-wget -O /tmp/fonts.zip https://download.jetbrains.com/fonts/${FONT_NAME}
+# Before fonts
+# FONT_NAME=JetBrainsMono-2.001.zip
+# wget -O /tmp/fonts.zip https://download.jetbrains.com/fonts/${FONT_NAME}
+# unzip -od /tmp/fonts/ /tmp/fonts.zip
+
+# FONT_DIR=/tmp/fonts/ttf
+# cp -v \
+#   $FONT_DIR/JetBrainsMono-Regular.ttf \
+#   $FONT_DIR/JetBrainsMono-Bold.ttf \
+#   $FONT_DIR/JetBrainsMono-Italic.ttf \
+#   $FONT_DIR/JetBrainsMono-Bold-Italic.ttf \
+#   ~/.local/share/fonts
+
+FONT_NAME=CascadiaCode-2009.22.zip
+wget -O /tmp/fonts.zip https://github.com/microsoft/cascadia-code/releases/download/v2009.22/${FONT_NAME}
 unzip -od /tmp/fonts/ /tmp/fonts.zip
 
 FONT_DIR=/tmp/fonts/ttf
 cp -v \
-  $FONT_DIR/JetBrainsMono-Regular.ttf \
-  $FONT_DIR/JetBrainsMono-Bold.ttf \
-  $FONT_DIR/JetBrainsMono-Italic.ttf \
-  $FONT_DIR/JetBrainsMono-Bold-Italic.ttf \
+  $FONT_DIR/CascadiaCode.ttf \
+  $FONT_DIR/CascadiaCodePL.ttf \
+  $FONT_DIR/CascadiaMono.ttf \
+  $FONT_DIR/CascadiaMonoPL.ttf \
+  $FONTDIR/static/*.ttf \
   ~/.local/share/fonts
