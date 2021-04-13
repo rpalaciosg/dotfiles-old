9ba340d01ffede4e77c865dd1b12770d4ba2e488
Richard Palacios G <apalaciosg91@gmail.com>
2021-04-13 10:34:06 -0500

    feat(colorls) : add script to install colorls

---
+++ scripts/colorls.sh
@@ -0,0 +1,6 @@
+# Install Ruby and dependences of colorls 
+sudo apt install -y ruby ruby-dev ruby-colorize
+sudo apt-get install -y libncurses5-dev libtinfo-dev
+
+# Install Rubygem colorls 
+sudo gem install colorls
